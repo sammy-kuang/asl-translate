2f6c94f68a7b1d10aeb9726a0d949c2e35268f84
Sammy Kuang <theliverman@protonmail.com>
2024-01-20 14:41:10 -0800

    add lettering, update requirements

--- backend/translate_service.py
+++ backend/translate_service.py
@@ -5,17 +5,26 @@ from scraper import download_video
 
 import ffmpeg
 
+def get_video_directory() -> str:
+    return os.path.dirname(os.path.realpath(__file__))
+
 # Returns list of path(s) to mp4 files that either ARE the word or SPELL OUT the word
 # Fail state is an empty array
 def get_word(word : str) -> []:
     out = []
-    if os.path.exists(os.path.join(os.path.realpath(__file__), word + ".mp4")):
-        url = query_for_word_online(word)
+    check_path = os.path.join(get_video_directory, word + ".mp4")
 
-        if url is None:
-            pass # TODO: Spell out word
-        else:
-            out.append(download_video(url, "videos/" + word + ".mp4"))
+    if os.path.exists(check_path):
+        out.append(check_path)
+        return out
+
+    url = query_for_word_online(word)
+
+    if url is not None:
+        out.append(download_video(url, "videos/" + word + ".mp4"))
+    else:
+        for letter in word.split():
+            out.append(os.path.join(get_video_directory(), "videos/" + letter.upper() + ".mp4"))
     return out
         
 
