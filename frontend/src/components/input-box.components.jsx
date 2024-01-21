60a3f97119163454dde52bd18ec5a431bf0ebb7e
Ernie Han <erniehan512@gmail.com>
2024-01-20 17:03:16 -0800

    first push from frontend, merge with backend

---
+++ frontend/src/components/input-box.components.jsx
@@ -0,0 +1,9 @@
+const InputBox = () => {
+    return (
+        <input className="input-box">
+            Enter the word you want to translate
+        </input>
+    )
+}
+
+export default InputBox
