b1c1d97401818fe1f4c67033d0c70530cbfde459
Sammy Kuang <theliverman@protonmail.com>
2024-01-21 00:23:11 -0800

    work on mongo integration

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import traceback
 from flask import Flask, request, jsonify, send_file
 from translate_service import translate, VIDEO_FORMAT, SERVER_ERROR_VIDEO
 from scraper import download_deps
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -23,6 +24,11 @@ def get_translate():
 
     return send_file(cached_video_path, mimetype="video/" + VIDEO_FORMAT)
     
+@app.route("/dummy", methods=['GET'])
+def dummy():
+    from mongo import FS, search_file_in_gridfs
+    video_file = FS.get(ObjectId(search_file_in_gridfs("0.mp4")))
+    return send_file(video_file, mimetype="video/mp4")
 
 if __name__ == "__main__":
     # run script with --download-deps to download the numbers and letters for ASL
