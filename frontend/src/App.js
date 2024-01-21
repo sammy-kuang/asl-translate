68b806a51efeedfdef4a4b50a4a89a86bc01d07a
Sammy Kuang <theliverman@protonmail.com>
2024-01-20 18:27:35 -0800

    implement basic video player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import InputBox from './components/input-box.components';
+import VideoPlayer from './components/video-player.components';
 
 function App() {
   return (
 
     <div className="App">
       <h1 className='head-text'>ASL Translate</h1>
-      <InputBox/>
+      <div className="side-by-side">
+        <InputBox />
+        <VideoPlayer />
+      </div>
     </div>
 
   );
